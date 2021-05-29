56aa119002fc7a6725903bdd102deb3714f0b479
lotan amit <lotan.amit@gmail.com>
2021-05-29 11:57:45 +0300

    test commit 1

---
+++ part1/ExamDetails.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "6! = " << factorial(5) << endl;
+}
