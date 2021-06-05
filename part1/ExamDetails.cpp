a330151f4a9d382ad79ce5843a318f1fc15a2b5a
lotan amit <lotan.amit@gmail.com>
2021-06-05 13:05:32 +0300

    update for pull day 1

--- part1/ExamDetails.cpp
+++ part1/ExamDetails.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
-using namespace std;
+#include "ExamDetails.h"
 
-int factorial(int n)
+ExamDetails::ExamDetails(int course_number, int month, int day, double hour, int length, std::string zoom_link = "")
+    : course_number(course_number), month(month), day(day), hour(hour), length(length), zoom_link(zoom_link) {}
+
+ExamDetails::ExamDetails(const ExamDetails &exam)
+    : course_number(exam.course_number), month(exam.month), day(exam.day), hour(exam.hour),
+      length(exam.length), zoom_link(exam.zoom_link) {}
+
+std::string ExamDetails::getLink() const
 {
-    if (n <= 1)
-    {
-        return 1;
-    }
-    return n * factorial(n - 1);
+    return zoom_link;
 }
 
-int main()
+void ExamDetails::setLink(std::string new_link)
 {
-    cout << "Hi world!" << endl;
-    cout << "6! = " << factorial(6) << endl;
+    zoom_link = new_link;
+}
+
+int ExamDetails::operator-(const ExamDetails &exam) const
+{
+    return (month - exam.month) * 30 + (day - exam.day);
+}
+
+bool ExamDetails::operator<(const ExamDetails &exam) const
+{
+    return (*this - exam == 0) ? (*this - exam) < 0 : (hour - exam.hour) < 0;
+}
+
+std::ostream &operator<<(std::ostream & os, const ExamDetails & exam)
+{
+}
+
+ExamDetails makeMatamExam()
+{
+    return ExamDetails(234124, 7, 28, 13.0, 3, "https://tinyurl.com/59hzps6m");
 }
--- part1/ExamDetails.h
+++ part1/ExamDetails.h
@@ -4,20 +4,22 @@
 
 class ExamDetails
 {
-private:
-    int course_number;
-    int month;
-    int day;
+    int course_number, month, day, length;
     double hour;
-    int length;
     std::string zoom_link;
 
 public:
     ExamDetails(int course_number, int month, int day, double hour, int length, std::string zoom_link = "");
-    ~ExamDetails();
+    ExamDetails(const ExamDetails &exam);
+    ~ExamDetails() = default;
+    ExamDetails &operator=(const ExamDetails &exam) = default;
+    std::string getLink() const;
+    void setLink(std::string new_link);
+    int operator-(const ExamDetails &) const;
+    bool operator<(const ExamDetails &) const;
+    friend std::ostream &operator<<(std::ostream &, const ExamDetails &);
 };
 
-
-std::string getLink();
+ExamDetails makeMatamExam();
 
 #endif
