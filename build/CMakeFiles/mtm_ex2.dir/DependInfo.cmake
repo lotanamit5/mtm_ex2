9cab347859754524aecbfe1ca567ed94d8d1fb89
nircoe <nircoe@gmail.com>
2021-06-06 13:14:57 +0300

    new

---
+++ build/CMakeFiles/mtm_ex2.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/USER/OneDrive - Technion/Desktop/Studying/Semester B/Introduce to system programming/mtm_ex2/part1/ExamDetails.cpp" "CMakeFiles/mtm_ex2.dir/part1/ExamDetails.cpp.obj" "gcc" "CMakeFiles/mtm_ex2.dir/part1/ExamDetails.cpp.obj.d"
+  "C:/Users/USER/OneDrive - Technion/Desktop/Studying/Semester B/Introduce to system programming/mtm_ex2/part1/part1_tests.cpp" "CMakeFiles/mtm_ex2.dir/part1/part1_tests.cpp.obj" "gcc" "CMakeFiles/mtm_ex2.dir/part1/part1_tests.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mtm_ex2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mtm_ex2.dir/part1/ExamDetails.cpp.obj"
+  "CMakeFiles/mtm_ex2.dir/part1/ExamDetails.cpp.obj.d"
+  "CMakeFiles/mtm_ex2.dir/part1/part1_tests.cpp.obj"
+  "CMakeFiles/mtm_ex2.dir/part1/part1_tests.cpp.obj.d"
+  "libmtm_ex2.dll.a"
+  "mtm_ex2.exe"
+  "mtm_ex2.exe.manifest"
+  "mtm_ex2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mtm_ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
