acc53e391741e9d84a1ba1cabfce6cd1804d03b9
lotan amit <lotan.amit@gmail.com>
2021-06-13 00:16:02 +0300

    shabat matam bitch

---
+++ partA/ExamDetails.h
@@ -0,0 +1,65 @@
+// #ifndef EXAMDETAILS_H_
+// #define EXAMDETAILS_H_
+// #include <iostream>
+
+// class ExamDetails
+// {
+//     int course_number, month, day, length;
+//     double hour;
+//     std::string zoom_link;
+
+// public:
+//     class InvalidDateException:std::exception{};
+//     class InvalidTimeException:std::exception{};
+//     class InvalidArgsException:std::exception{};
+//     ExamDetails(int course_number, int month, int day, double hour, int length, std::string zoom_link = "");
+//     ExamDetails(const ExamDetails &);
+//     ~ExamDetails() = default;
+//     ExamDetails &operator=(const ExamDetails &exam) = default;
+//     std::string getLink() const;
+//     void setLink(std::string new_link);
+//     int operator-(const ExamDetails &) const;
+//     bool operator<(const ExamDetails &) const;
+//     friend std::ostream &operator<<(std::ostream &, const ExamDetails &);
+//     static ExamDetails makeMatamExam();
+// };
+
+// #endif
+
+#ifndef EXAMDETAILS_H_
+#define EXAMDETAILS_H_
+
+#include <string>
+
+namespace mtm
+{
+    class ExamDetails
+    {
+        int course_number, month, day, length;
+        double hour;
+        std::string zoom_link;
+
+    public:
+        class InvalidDateException
+        {
+        };
+        class InvalidTimeException
+        {
+        };
+        class InvalidArgsException
+        {
+        };
+        ExamDetails(int course_number, int month, int day, double hour, int length, std::string zoom_link = "");
+        ExamDetails(const ExamDetails &exam);
+        ~ExamDetails() = default;
+        ExamDetails &operator=(const ExamDetails &exam) = default;
+        std::string getLink() const;
+        void setLink(const std::string new_link);
+        int operator-(const ExamDetails &) const;
+        bool operator<(const ExamDetails &) const;
+        friend std::ostream &operator<<(std::ostream &os, const ExamDetails &exam);
+        static ExamDetails makeMatamExam();
+    };
+}
+
+#endif
