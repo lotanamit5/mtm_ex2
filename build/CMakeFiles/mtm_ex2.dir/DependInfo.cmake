0915b8916e68f76239b4e1b95e479ae67078cbaf
Nir Cohen <nircoe@localhost.localdomain>
2021-06-12 01:52:02 -0700

    new

---
+++ build/CMakeFiles/mtm_ex2.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nircoe/Desktop/mtm_ex2/part1/ExamDetails.cpp" "/home/nircoe/Desktop/mtm_ex2/build/CMakeFiles/mtm_ex2.dir/part1/ExamDetails.cpp.o"
+  "/home/nircoe/Desktop/mtm_ex2/part1/part1_tests.cpp" "/home/nircoe/Desktop/mtm_ex2/build/CMakeFiles/mtm_ex2.dir/part1/part1_tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
